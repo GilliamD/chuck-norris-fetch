1a9bde5e46bb5d5345a2b21159abb0dabacef7b7
Gilford Williams <GilliamDX@gmail.com>
2019-09-23 13:12:41 -0400

    initial commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,26 @@
 "use strict";
 
 const getMoreQuotesButton = document.getElementById("getMoreQuotes");
+const chuckSays = document.getElementById("chuckSays");
 
 // Add an event listener to the button, DON'T FORGET TO PREVENT THE DEFAULT BEHAVIOR!
 // Call a function to return a new quote, and update the DOM
-getMoreQuotesButton.addEventListener("click", function(e) {});
+getMoreQuotesButton.addEventListener("click", function(e) {
+    console.log("button clicked");
+    updateChuckSays("dev");
+});
 
-// Create a function to update the quote text in the DOM
-function updateChuckSays() {}
+//Create a function to update the quote in the text
+function updateChuckSays(category) {
+//This object is a promise like today
+    const chuckQuote = get(
+    `https://api.chucknorris.io/jokes/random?category=${category}`
+    );
+
+    chuckQuote.then(function(quote) {
+        chuckSays.innerHTML = quote.value;
+    });
+}
+updateChuckSays("food");
+
+//Chuck Norris can write infinte recursion functions and have them return.

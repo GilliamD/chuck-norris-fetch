d8ef8131ffc78937dc1777746dea59f5947b16ad
Gilford Williams <GilliamDX@gmail.com>
2019-09-24 13:07:31 -0400

    Refactored for dynamically populating DOM

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,12 @@
 "use strict";
 
-const getMoreQuotesButton = document.getElementById("getMoreQuotes");
-const chuckSays = document.getElementById("chuckSays");
+const chuckQuoteForm = document.querySelector("#chuckQuotesForm")
 
-// Add an event listener to the button, DON'T FORGET TO PREVENT THE DEFAULT BEHAVIOR!
-// Call a function to return a new quote, and update the DOM
-getMoreQuotesButton.addEventListener("click", function(e) {
-    console.log("button clicked");
-    updateChuckSays("dev");
+chuckQuoteForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const categoryValue = chuckQuoteForm.querySelector("select").value;
+    updateChuckSays(categoryValue);
 });
-
 //Create a function to update the quote in the text
 function updateChuckSays(category) {
 //This object is a promise like today
@@ -21,6 +18,29 @@ function updateChuckSays(category) {
         chuckSays.innerHTML = quote.value;
     });
 }
-updateChuckSays("food");
+//Using get function to fetch data
+function getCategories() {
+    const selectWrapper = document.querySelector("#selectWrapper");
+    const categoryList = document.createElement("select");
+    
+    get(`https://api.chucknorris.io/jokes/categories`).then(function(response){
+        //returns the array into a list of individual values
+        response.forEach(function(category) {
+            const categoryOption = document.createElement("option");
+            categoryOption.text = category;
+            categoryOption.value = category;
+            categoryList.append(categoryOption);
+        });
+    });
+    //attaches grabbed list to new variable
+    selectWrapper.append(categoryList);
+}
+// I(mmediately.I(nvoked.F(unction.E(xpression goes here. An annoymus funcion that just runs
+
+(function() {
+    const defaultCategory = 'dev';
+    getCategories();
+    updateChuckSays(defaultCategory);
+})();
 
 //Chuck Norris can write infinte recursion functions and have them return.
